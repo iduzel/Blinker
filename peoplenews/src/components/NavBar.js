c0bf641103d6a15ba9a1a91da2270a121a2982d0
ismail <iduzel49@gmail.com>
2022-03-07 13:28:47 +0100

    navbar added

---
+++ peoplenews/src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Nav } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div>
+        <Nav className='container d-flex '>
+            <Link to="/register">REGISTER</Link>
+            <Link className='ms-2 me-2' to="/">LOGIN</Link>
+            <Link to="/home">HOME</Link>
+        </Nav>
+    </div>
+  )
+}
+
+export default Navbar
