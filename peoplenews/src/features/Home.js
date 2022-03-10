dc214ac1cc415cb7a3de67b6e69a39997e1265cc
ismail <iduzel49@gmail.com>
2022-03-10 14:16:30 +0100

    fixed login

--- peoplenews/src/features/Home.js
+++ peoplenews/src/features/Home.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useContext, useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { UserContext } from '../components/context';
 import Blink from './Blink'
 
 const Home = () => {
