348d8c811cea305c109d70e79a351d9acb98fd5d
ismail <iduzel49@gmail.com>
2022-03-12 23:50:07 +0100

    logout added

--- peoplenews/src/components/NavBar.js
+++ peoplenews/src/components/NavBar.js
@@ -1,17 +1,42 @@
-import React from 'react'
-import { Nav } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import React, { useContext } from "react";
+import { Button, Col, Nav, Row } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
+import { UserContext } from "./context";
 
 const Navbar = () => {
-  return (
-    <div className='bg-danger fs-1 fw-bold'>
-        <Nav className='container d-flex '>
-            <Link className='navbar-link' to="/register">REGISTER</Link>
-            <Link className='navbar-link ms-5 me-5' to="/">LOGIN</Link>
-            <Link  className='navbar-link' to="/home">HOME</Link> 
-        </Nav>
-    </div>
-  )
-}
+  const navigate = useNavigate();
+  const { userData, setUserData } = useContext(UserContext);
 
-export default Navbar
+  const handleLogout = () => {
+    // clear the context
+    setUserData(null);
+    // redirect user to home
+    navigate("/");
+  };
+  return (
+    <div className="bg-danger fs-1 fw-bold">
+      <Row>
+        <Col className="col-11">
+          <Nav className="container d-flex ">
+            <Link className="navbar-link" to="/register">
+              REGISTER
+            </Link>
+            <Link className="navbar-link ms-5 me-5" to="/">
+              LOGIN
+            </Link>
+            <Link className="navbar-link" to="/home">
+              HOME
+            </Link>
+          </Nav>
+        </Col>
+        <Col className="col-1">
+        <p>
+        <Button onClick={handleLogout}>Logout</Button>
+      </p>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default Navbar;
