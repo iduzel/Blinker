f29a1069135e83c4647be537480eb7059df138aa
ismail <iduzel49@gmail.com>
2022-03-14 13:43:39 +0100

    added username

--- peoplenews/src/components/context.js
+++ peoplenews/src/components/context.js
@@ -5,9 +5,10 @@ export const UserContext = createContext();
 export default function UserProvider({ children }) {
   const [userData, setUserData] = useState(null);
   const [showClass, setShowClass] = useState('hide')
+  const [username, setUsername] = useState('')
 
   return (
-    <UserContext.Provider value={{ userData, setUserData, showClass, setShowClass }}>
+    <UserContext.Provider value={{ userData, setUserData, showClass, setShowClass, username, setUsername }}>
       {children}
     </UserContext.Provider>
   );
