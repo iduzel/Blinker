40b4f18b8b8549fe638612f2c7782d090476173d
ismail <iduzel49@gmail.com>
2022-03-14 15:53:41 +0100

    added delete button class

--- peoplenews/src/components/context.js
+++ peoplenews/src/components/context.js
@@ -6,9 +6,10 @@ export default function UserProvider({ children }) {
   const [userData, setUserData] = useState(null);
   const [showClass, setShowClass] = useState('hide')
   const [username, setUsername] = useState('')
+  const [userId, setUserId] = useState('')
 
   return (
-    <UserContext.Provider value={{ userData, setUserData, showClass, setShowClass, username, setUsername }}>
+    <UserContext.Provider value={{ userData, setUserData, showClass, setShowClass, username, setUsername, userId, setUserId }}>
       {children}
     </UserContext.Provider>
   );
