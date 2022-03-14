e0effd7bb48f773927a9757e9787cbc876c4697d
ismail <iduzel49@gmail.com>
2022-03-14 13:43:54 +0100

    added logout

--- peoplenews/src/components/NavBar.js
+++ peoplenews/src/components/NavBar.js
@@ -1,13 +1,14 @@
-import React, { useContext } from "react";
-import { Button, Col, Nav, Row } from "react-bootstrap";
+import React, { useContext, useState } from "react";
+import { Button, Col, Nav, OverlayTrigger, Row } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { UserContext } from "./context";
 import logo from "../components/pictures/5.jpg";
-import '../features/features.scss'
+import "../features/features.scss";
 
 const Navbar = () => {
+  const [logoutClass, setLogoutClass] = useState("hide");
   const navigate = useNavigate();
-  const { userData, setUserData, showClass, setShowClass } =
+  const { userData, setUserData, showClass, setShowClass, username } =
     useContext(UserContext);
 
   const handleLogout = () => {
@@ -17,16 +18,35 @@ const Navbar = () => {
     navigate("/");
     setShowClass("hide");
   };
+
+  const handleUsernameLogout = () => {
+    setLogoutClass("show");
+  };
   return (
     <div className="navbar-logo fs-1 fw-bold mt-5 shadow-lg">
       <Row>
-        <Col className="col-11">
+        <Col className="col-10">
           <Nav className="container d-flex ">
             <img className="logo" src={logo} alt="logo" />
           </Nav>
         </Col>
-        <Col className="col-1">
-          <Button className={showClass} onClick={handleLogout}>
+
+        <Col
+          onMouseLeave={() => setLogoutClass("hide")}
+          className="col-2  d-flex flex-column justify-content-center align-items-center "
+        >
+          <h2
+            style={{ color: "#61B7DE" }}
+            onMouseOver={() => setLogoutClass("show")}
+            className={`${showClass} demo`}
+          >
+            {username}{" "}
+          </h2>
+          <Button
+            onMouseLeave={() => setLogoutClass("hide")}
+            className={logoutClass}
+            onClick={handleLogout}
+          >
             Logout
           </Button>
         </Col>
