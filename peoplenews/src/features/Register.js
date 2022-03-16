ef7a14a12d5721c695a8eafa0a8712431a49b326
ismail <iduzel49@gmail.com>
2022-03-16 11:25:06 +0100

    added image

--- peoplenews/src/features/Register.js
+++ peoplenews/src/features/Register.js
@@ -1,5 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { Button, Container, Form, FormText } from "react-bootstrap";
+import {
+  Button,
+  Col,
+  Container,
+  Form,
+  FormText,
+  Image,
+  Row,
+} from "react-bootstrap";
 import "./features.scss";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
@@ -23,56 +31,70 @@ const Register = () => {
   };
   return (
     <div className="container register">
-      <Container>
-        <Form onSubmit={(e) => handleSubmit(e)}>
-          <Form.Group className="mb-4">
-            <Form.Label>Username</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Enter a username"
-              value={data.username}
-              onChange={(e) => setData({ ...data, username: e.target.value })}
-            />
-            <Form.Text className="text-muted"></Form.Text>
-          </Form.Group>
-          <Form.Group className="mb-4">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control
-              type="email"
-              placeholder="Enter email"
-              value={data.email}
-              onChange={(e) => setData({ ...data, email: e.target.value })}
-            />
-            <Form.Text className="text-muted"></Form.Text>
-          </Form.Group>
+      <Row>
+        <Col>
+          <Image
+            className="h-75 w-100 rounded"
+            fluid
+            src="https://images.unsplash.com/photo-1570563568161-e4f5e8c6e27f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8cmVwb3J0ZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+          />
+        </Col>
+        <Col>
+          {" "}
+          <Form onSubmit={(e) => handleSubmit(e)}>
+            <Form.Group className="mb-4">
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder=" Enter a username"
+                value={data.username}
+                onChange={(e) => setData({ ...data, username: e.target.value })}
+              />
+              <Form.Text className="text-muted"></Form.Text>
+            </Form.Group>
+            <Form.Group className="mb-4">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder=" Enter email"
+                value={data.email}
+                onChange={(e) => setData({ ...data, email: e.target.value })}
+              />
+              <Form.Text className="text-muted"></Form.Text>
+            </Form.Group>
 
-          <Form.Group className="mb-4">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder="Password"
-              value={data.pass}
-              onChange={(e) => setData({ ...data, password: e.target.value })}
-            />
-          </Form.Group>
-          <Form.Group className="mb-4">
-            <Form.Label>Confirm Password</Form.Label>
-            <Form.Control type="password" placeholder="Confirm Password" />
-          </Form.Group>
+            <Form.Group className="mb-4">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder=" Password"
+                value={data.pass}
+                onChange={(e) => setData({ ...data, password: e.target.value })}
+              />
+            </Form.Group>
+            <Form.Group className="mb-4 ">
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control type="password" placeholder="  Confirm Password" />
+            </Form.Group>
 
-          <Button
-            className="text-danger fs-5 fw-bold border-2 bg-info"
-            variant="outline-danger"
-            type="submit"
-          >
-            Register
-          </Button>
-        </Form>
-      </Container>
-
-      <Form.Group className="mt-3 container">
-        <FormText>Already have an account! <small><Link to='/'>Login</Link></small></FormText>
-      </Form.Group>
+            <Button
+              className="color register-button fs-5 fw-bold border-2 "
+              variant=""
+              type="submit"
+            >
+              Register
+            </Button>
+          </Form>
+          <Form.Group className="mt-5 container">
+            <FormText>
+              Already have an account!{" "}
+              <small>
+                <Link to="/">Login</Link>
+              </small>
+            </FormText>
+          </Form.Group>
+        </Col>
+      </Row>
     </div>
   );
 };
