6ba21b380dc79fe64e1076f4341290325a617542
ismail <iduzel49@gmail.com>
2022-03-17 13:12:47 +0100

    d

--- peoplenews/src/components/CardBlinkDemo.js
+++ peoplenews/src/components/CardBlinkDemo.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 import React, { useContext, useState } from "react";
-import { Button, Card, Image } from "semantic-ui-react";
-import Img4 from "./pictures/4.jpg";
+import { Card, Image } from "semantic-ui-react";
 import "../features/features.scss";
 import { UserContext } from "./context";
 
-const CardBlinkDemo = (props) => {
 
+const CardBlinkDemo = (props) => {
   const { userId, userData, setUserData } = useContext(UserContext);
   const [singleBlinkClass, setSingleBlankClass] = useState("hideDelete");
 
@@ -30,19 +29,16 @@ const CardBlinkDemo = (props) => {
     }
   };
 
-  const handleUserBlinks = (id) =>  {
+  const handleUserBlinks = (id) => {
+    console.log(("id", id));
 
-    console.log(('id', id))
-
-   props.setBlinks(props.blinks.filter((item, index) => item.owner._id == id)) 
-  }
-
+    props.setBlinks(props.blinks.filter((item, index) => item.owner._id == id));
+  };
 
   const handleLikeClick = async (postid) => {
+    console.log("postid: ", postid);
+    console.log("userData_id is : ", userData._id);
 
-    console.log('postid: ', postid)
-    console.log('userData_id is : ', userData._id)
-    
     const response = await axios.put(
       `/blinks/likeadd/${postid}/${userData._id}`
     );
@@ -51,8 +47,8 @@ const CardBlinkDemo = (props) => {
 
     if (response.data.success) {
       const postIdx = props.blinks.findIndex((item) => item._id == postid);
-      console.log('postIdx', postIdx)
-      console.log('item._id',props.blinks[postIdx]._id)
+      console.log("postIdx", postIdx);
+      console.log("item._id", props.blinks[postIdx]._id);
 
       const oldPosts = [...props.blinks];
 
@@ -60,17 +56,22 @@ const CardBlinkDemo = (props) => {
 
       props.setBlinks([...oldPosts]);
     }
-  }
+  };
 
- 
-  
 
   return (
     <Card.Group className=" card-blink-demo w-100 d-flex">
       <Card className=" m-0 w-100">
         <Card.Content>
-          <Image className="card-img" floated="left" src={props.item.owner.image} />
-          <Card.Header className="hover" onClick={() => handleUserBlinks(props.item.owner._id)} >
+          <Image
+            className="card-img"
+            floated="left"
+            src={props.item.owner.image}
+          />
+          <Card.Header
+            className="hover"
+            onClick={() => handleUserBlinks(props.item.owner._id)}
+          >
             {props.item.owner.username}
           </Card.Header>
           <Card.Meta>{props.item.owner.email}</Card.Meta>
@@ -88,10 +89,8 @@ const CardBlinkDemo = (props) => {
             </p>
             <div className={`${singleBlinkClass} deleteText `}>
               {/* className = "test abc demo " */}
-              <p >Edit</p>
-              <p onClick={() => handleDelete(props.item._id)} >
-                Delete
-              </p>
+              <p>Edit</p>
+              <p onClick={() => handleDelete(props.item._id)}>Delete</p>
             </div>
           </Card.Description>
 
@@ -100,25 +99,32 @@ const CardBlinkDemo = (props) => {
           </Card.Description>
         </Card.Content>
         <Card.Content extra>
-        <hr style={{ width: "90%" }} />
-          <div style={{ display: "flex", justifyContent: "" }}>
-            <span
-              style={{
-                cursor: "pointer",
-             color: props.item.likes.includes(userData._id) ? "red" : "black" 
-              }}
-              onClick={() => handleLikeClick(props.item._id)}
-            >
-              <i className="heart outline icon"></i>
-            </span>
-            
-            <div>{props.item.likes.length}</div>
-           
+          <hr style={{ width: "90%" }} />
+          <div className="d-flex justify-content-between">
+            <div className="d-flex justify-content-between">
+              {" "}
+              <span
+                style={{
+                  cursor: "pointer",
+                  color: props.item.likes.includes(userData._id)
+                    ? "red"
+                    : "black",
+                }}
+                onClick={() => handleLikeClick(props.item._id)}
+              >
+                <i className="heart outline icon"></i>
+              </span>
+              <div>{props.item.likes.length}</div>
+            </div>
+            <div><small>Comments: 0</small>   </div>         
           </div>
-          <div>Comments: 0</div>
         </Card.Content>
       </Card>
+
+    
     </Card.Group>
+
+   
   );
 };
 
